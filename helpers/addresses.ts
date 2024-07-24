98e705f1ed984ef5dd2ba0aeb69c075f9147187e
zpnst <akuratnonenado@gmail.com>
2024-07-24 15:16:03 +0300

    add addresses

--- helpers/addresses.ts
+++ helpers/addresses.ts
@@ -3,7 +3,7 @@ export const ROOT_DNS_ADDRESS: string = "EQBqg8DYYY9ujKoIqGBJv0BXnyB-km2Hv8BEKzN
 
 export const DUCK_MINTER_ADDRESS: string = "kQAHCWupGXRPAFdiFikxpzqm4ruVHhWscx9qx51031hThP1n"; // testnet
 
-export const DUCK_DOMAIN_NAMES_MINTER: string = "EQAONWfT-Sa5R2r9f-RU8W_Iu_aeTtoEHbLO2R7aJ4TaLCTu"; // testnet
-export const DNS_ITEM_ADDRESS: string = "kQCyoAvCqwnC2ohgRt-MyBU_M5u_Qo4XbMF9Iw0JYqYkn7gT"; // testnet
+export const DUCK_DOMAIN_NAMES_MINTER: string = "EQBfGuVdRUtwKzkb4REw6hy3zShrAEZtBVmAlOfL1WNf_gal"; // testnet
+export const DNS_ITEM_ADDRESS: string = "kQDFsZXNS-Yk1AyNTGrbLmyoQZucwlCNhu7dx20HMKncCiNS"; // testnet
 
 // export const DUCK_MINTER_ADDRESS: string = "EQBTcytZjdZwFLj6TO7CeGvn4aMmqXvbX-nODiApbd011gT3"; // maonnet
