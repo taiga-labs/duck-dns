4796b0e55e2989177ecac9db4f041f57da4d4298
zpnst <akuratnonenado@gmail.com>
2024-07-25 15:15:29 +0300

    деплой в тестнет, изменил адреса в файле helpers/addresses.ts

--- helpers/addresses.ts
+++ helpers/addresses.ts
@@ -3,7 +3,7 @@ export const ROOT_DNS_ADDRESS: string = "EQBqg8DYYY9ujKoIqGBJv0BXnyB-km2Hv8BEKzN
 
 export const DUCK_MINTER_ADDRESS: string = "kQAHCWupGXRPAFdiFikxpzqm4ruVHhWscx9qx51031hThP1n"; // testnet
 
-export const DUCK_DOMAIN_NAMES_MINTER: string = "EQDEcvEUrJIYG14cWf27nJf4e4xp4ozLyp5Gnhlwrn4ewpTN"; // testnet
-export const DNS_ITEM_ADDRESS: string = "kQBNB0izmuAOerKAs-BSgntejnK14P9kNQjjI-97wx8M8hkq"; // testnet
+export const DUCK_DOMAIN_NAMES_MINTER: string = "EQAVMH0yDTfUzQ0zGa5iiWz3eyADcA1ppay6r8TE9q1oHFw1"; // testnet
+export const DNS_ITEM_ADDRESS: string = "0QB_e1KbRE6ftcfKfO2vMDuLAm7MAGSFI6QmMdZz4bp_wEdM"; // testnet
 
 // export const DUCK_MINTER_ADDRESS: string = "EQBTcytZjdZwFLj6TO7CeGvn4aMmqXvbX-nODiApbd011gT3"; // maonnet
