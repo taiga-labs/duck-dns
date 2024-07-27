539a9906131396a719ed7d8a88c8aa85801b0136
zpnst <akuratnonenado@gmail.com>
2024-07-27 23:36:46 +0300

    теперь остаток TON отправляется на контракт коллекции за вычетом TON, нужных для 'хранения'контракта в сети, на контракт коллекции

--- helpers/addresses.ts
+++ helpers/addresses.ts
@@ -1,9 +1,9 @@
 
-export const ROOT_DNS_ADDRESS: string = "EQBqg8DYYY9ujKoIqGBJv0BXnyB-km2Hv8BEKzN48KSwaHrq"; // testnet
+export const ROOT_DNS_ADDRESS: string = "EQAUcpXm45FvnZI2j7yO4q2r8wRQ2gqO9WdKSjIh7IzmE5zc"; // testnet
 
 export const DUCK_MINTER_ADDRESS: string = "kQAHCWupGXRPAFdiFikxpzqm4ruVHhWscx9qx51031hThP1n"; // testnet
 
-export const DUCK_DOMAIN_NAMES_MINTER: string = "EQAVMH0yDTfUzQ0zGa5iiWz3eyADcA1ppay6r8TE9q1oHFw1"; // testnet
-export const DNS_ITEM_ADDRESS: string = "0QB_e1KbRE6ftcfKfO2vMDuLAm7MAGSFI6QmMdZz4bp_wEdM"; // testnet
+export const DUCK_DOMAIN_NAMES_MINTER: string = "EQDcxv04mmwkY326o_bTAxIWb3Zaq7Ie1fV1GwG2YUBXBc8T"; // testnet
+export const DNS_ITEM_ADDRESS: string = "kQCXItTAv8CI1Hz_dS7LRh7kO1JiaKnbcFy5SYf6TlU8lxdi"; // testnet
 
 // export const DUCK_MINTER_ADDRESS: string = "EQBTcytZjdZwFLj6TO7CeGvn4aMmqXvbX-nODiApbd011gT3"; // maonnet
