9488bf48c881dd7e297251394b6af346263b286a
zpnst <akuratnonenado@gmail.com>
2024-07-28 08:52:13 +0300

    поменял минимальное разрешённое количество символов в доменном имени на 1

--- helpers/addresses.ts
+++ helpers/addresses.ts
@@ -3,7 +3,7 @@ export const ROOT_DNS_ADDRESS: string = "EQAUcpXm45FvnZI2j7yO4q2r8wRQ2gqO9WdKSjI
 
 export const DUCK_MINTER_ADDRESS: string = "kQAHCWupGXRPAFdiFikxpzqm4ruVHhWscx9qx51031hThP1n"; // testnet
 
-export const DUCK_DOMAIN_NAMES_MINTER: string = "EQDcxv04mmwkY326o_bTAxIWb3Zaq7Ie1fV1GwG2YUBXBc8T"; // testnet
-export const DNS_ITEM_ADDRESS: string = "kQCXItTAv8CI1Hz_dS7LRh7kO1JiaKnbcFy5SYf6TlU8lxdi"; // testnet
+export const DUCK_DOMAIN_NAMES_MINTER: string = "EQDJFJEetoMIXTC0bW09Bd21yuHdJY_23pD3iewIwoC4hzv5"; // testnet
+export const DNS_ITEM_ADDRESS: string = "0QCa3eqkUz2s0c8LC10EtQ-AkV3_cE0Hx6qS_PgoeghKxP5n"; // testnet
 
 // export const DUCK_MINTER_ADDRESS: string = "EQBTcytZjdZwFLj6TO7CeGvn4aMmqXvbX-nODiApbd011gT3"; // maonnet
